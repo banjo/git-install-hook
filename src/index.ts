e20830ba19b07a7438594fc66feed22f22363859
Anton Ödman <anton.odman@gmail.com>
2023-03-14 21:19:25 +0100

    chore: whole process

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+import { objectKeys } from "@banjoanton/utils";
 import { cli } from "cleye";
 import { default as createDebugger } from "debug";
 import { globby } from "globby";
+import prompts from "prompts";
 import { version } from "../package.json";
 import { command } from "./command";
 
@@ -50,11 +52,10 @@ const main = async (args: CliType) => {
 
     debug(`Found lock files: ${lockFiles}`);
 
-    let selectedLockFile;
+    let selectedLockFile = lockFileMap[defaultPackageManager];
 
     if (lockFiles.length === 0 || lockFiles.length > 1) {
         debug(`No (or multiple) lock file(s) found, using ${defaultPackageManager} as default`);
-        selectedLockFile = lockFileMap[defaultPackageManager];
     }
 
     if (lockFiles.length === 1) {
@@ -62,12 +63,63 @@ const main = async (args: CliType) => {
         debug(`Using single lock file: ${selectedLockFile}`);
     }
 
-    const res = await command(`git diff --name-only HEAD@{1} HEAD | grep ${selectedLockFile}`);
+    const selectedPackageManager = objectKeys(lockFileMap).find(
+        key => lockFileMap[key] === selectedLockFile
+    );
 
-    if (!res) {
+    if (!selectedPackageManager) {
+        debug(`Could not find package manager for lock file ${selectedLockFile}, exiting`);
+        throw new Error(`Could not find package manager for lock file ${selectedLockFile}`);
+    }
+
+    const diffCommand = await command(`git diff --name-only HEAD@{1} HEAD`);
+
+    if (!diffCommand) {
         console.log("Could not execute git command, exiting");
         process.exit(1);
     }
+
+    const diff = diffCommand.stdout;
+    if (!diff.includes(selectedLockFile)) {
+        debug(`No changes to ${selectedLockFile}, exiting`);
+        // process.exit(0);
+    }
+
+    if (args.flags.prompt) {
+        console.log(`📦 Changes detected to ${selectedLockFile}, do you want to install?`);
+        const answer = await prompts({
+            type: "confirm",
+            name: "value",
+            message: "Do you want to install now?",
+            initial: true,
+        });
+
+        if (!answer.value) {
+            debug("User declined to install, exiting");
+            process.exit(0);
+        }
+    }
+
+    const installCommandMap = {
+        npm: "npm ci",
+        yarn: "yarn install --pure-lockfile",
+        pnpm: "pnpm install --frozen-lockfile",
+    };
+
+    const installCommand = installCommandMap[selectedPackageManager];
+
+    if (!installCommand) {
+        debug(
+            `Could not find install command for package manager ${selectedPackageManager}, exiting`
+        );
+        throw new Error(
+            `Could not find install command for package manager ${selectedPackageManager}`
+        );
+    }
+
+    debug(`Running install command: ${installCommand}`);
+
+    await command(installCommand, { stdio: "inherit" });
 };
 
 main(argv);
