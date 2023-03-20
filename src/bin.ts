abb5d40f89531c1d16d26068ad875ef6b7d8553a
Anton Ödman <anton.odman@gmail.com>
2023-03-20 20:56:15 +0100

    refactor: better naming

---
+++ src/bin.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { run } from ".";
+import { argv } from "./cli";
+
+run(argv);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-#!/usr/bin/env node
+import { default as createDebugger } from "debug";
+import pc from "picocolors";
+import { installDependencies, promptForInstall } from "./actions";
+import { CliType } from "./cli";
+import { hasChanged } from "./git";
+import { getPackageManager } from "./package-manager";
 
-import { run } from "./app";
-import { argv } from "./cli";
+export const run = async (args: CliType) => {
+    if (args.flags.debug) createDebugger.enable("*");
 
-run(argv);
+    const { noText, installation } = args.flags;
+    const showText = !noText;
+
+    const { lockFile, packageManager } = await getPackageManager();
+
+    if (!(await hasChanged(lockFile))) {
+        if (showText) console.log(pc.green("📦 No changes to lock file"));
+        process.exit(0);
+    }
+
+    if (args.flags.prompt) {
+        const shouldInstall = await promptForInstall(lockFile);
+        if (!shouldInstall) process.exit(0);
+    } else if (showText) {
+        console.log(pc.yellow(`📦 Changes detected to ${lockFile}`));
+    }
+
+    await installDependencies(packageManager, installation);
+    if (showText) console.log(pc.green("✅ Installation done!"));
+    process.exit(0);
+};
